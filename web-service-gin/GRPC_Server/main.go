4e48cc3f51d491bc87a653ed5df7583d5c48a280
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2021-10-04 14:45:21 +0200

    Succesfully added GRPC files and file structures

--- web-service-gin/GRPC_Server/main.go
+++ web-service-gin/GRPC_Server/main.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb "../GRPC_Protocol"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.CourseMethodsServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func (s *server) postCourse(ctx context.Context, in *pb.PostCourseRequest) (*pb.PostCourseReply, error){
+	log.Printf("Received: id:%s, studentRating:%v, name:", in.GetID())
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterCourseMethodsServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
